97daf0f35ff0019589aec3b664c15d77dd044e8b
Peter Hintze <hintze.peter@gmail.com>
2025-03-02 00:58:38 -0500

    final fix i think

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,8 +32,8 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (_req: Request, res: Response) => {
       res.sendFile(path.join(__dirname, '../client/build/index.html'));
