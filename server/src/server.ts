4b16fad78bfb9721bc60462f4ea0757bf14b5199
Peter Hintze <hintze.peter@gmail.com>
2025-03-02 01:23:15 -0500

    updated file path

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,10 +33,10 @@ const startApolloServer = async () => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
