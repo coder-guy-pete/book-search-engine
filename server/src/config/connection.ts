b6261a4adf36636cca8f24eba779794258d54a8e
Peter Hintze <hintze.peter@gmail.com>
2025-03-01 23:34:37 -0500

    connection file fixed

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 import mongoose from 'mongoose';
 
@@ -12,6 +18,7 @@ const db = async (): Promise<typeof mongoose.connection> => {
         return mongoose.connection;
     } catch (error) {
         console.log('Error connecting to database: ', error);
+        console.log('MONGODB_URI: ', MONGODB_URI);
         throw new Error('Database connection failed');  
     }
 };
